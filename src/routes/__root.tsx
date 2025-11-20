7ea3e232d4b3b6a46ba4b89db449dfcea7c4c38c
maj-rf <rmkfermin@gmail.com>
2025-11-20 12:07:45 +0800

    feat: add custom head & title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,19 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import Header from '../components/Header'
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { name: 'description', content: 'Stella Sore Potential Builder' },
+      { title: 'Noya' },
+    ],
+  }),
   component: () => (
     <>
+      <HeadContent />
       <Header />
       <Outlet />
       <TanStackDevtools
