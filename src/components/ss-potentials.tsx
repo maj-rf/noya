5d87637ef5175964a010926209a731fac12c934f
maj-rf <rmkfermin@gmail.com>
2025-11-25 12:03:36 +0800

    fix: uninstall item and separator ui, create potentials popover

--- src/components/ss-potentials.tsx
+++ src/components/ss-potentials.tsx
@@ -3,8 +3,13 @@ import { X } from 'lucide-react'
 import ResponsivePotential from './responsive-potential'
 import { Slider } from './ui/slider'
 import { Button } from './ui/button'
+import { ScrollArea, ScrollBar } from './ui/scroll-area'
 import type { SSPotential, SelectedPotential } from '@/types'
-import { Item, ItemGroup } from '@/components/ui/item'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 
 function SSPotentials({
   potentials,
@@ -62,76 +67,92 @@ function SSPotentials({
   }
 
   return (
-    <div className="pb-2">
-      <h1 className="sm:max-w-5/6 w-full mx-auto px-4">
-        {k === 'main' ? 'Main' : 'Support'}
-      </h1>
-      <ItemGroup className="flex-row overflow-x-scroll min-h-[170px] gap-1 p-2 sm:max-w-5/6 w-full mx-auto bg-blue-200">
-        {!selected || selected.length === 0 ? (
-          <div className="self-center justify-self-center w-full">
-            Please choose potentials below
-          </div>
-        ) : (
-          selected
-            .sort((a, b) => a.rarity - b.rarity)
-            .map((s) => (
-              <Item key={'selected' + s.id} className="flex flex-col p-0">
-                <div className="relative">
+    <div className="w-full max-w-11/12 mx-auto mb-2">
+      <Popover>
+        <PopoverTrigger>
+          <Button variant="outline" className="mb-2">
+            Add {k === 'main' ? 'Main' : 'Support'} Potentials
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-md md:w-2xl" side="top" align="start">
+          <ScrollArea className="w-full">
+            <div className="flex w-max my-2">
+              {filteredPotentials.map((p) => (
+                <div
+                  onClick={() => {
+                    addPotential(p)
+                  }}
+                  data-disabled={coreExceed}
+                  className={`rarity-${p.rarity} p-0`}
+                  key={p.id}
+                >
                   <ResponsivePotential
-                    key={'selected' + s.imgId + s.id}
-                    bgSrc={`./ss-vestige/vestige_${s.rarity}.png`}
-                    iconSrc={`https://res.cloudinary.com/dafqr01it/image/upload/v1763084273/ss/potential/${s.imgId}_A.png`}
-                    name={s.name}
+                    key={p.imgId + p.id}
+                    bgSrc={`./ss-vestige/vestige_${p.rarity}.png`}
+                    iconSrc={`https://res.cloudinary.com/dafqr01it/image/upload/v1763084273/ss/potential/${p.imgId}_A.png`}
+                    name={p.name}
                   />
-                  {s.rarity !== 0 && (
-                    <div className="absolute -top-0.5 left-3 text-sm font-semibold text-indigo-500">
-                      {s.level}
-                    </div>
-                  )}
-                  <Button
-                    variant="destructive"
-                    size="icon"
-                    className="absolute -top-1 -right-1 rounded-full size-5 border"
-                    onClick={() => removePotential(s.id)}
-                  >
-                    <X />
-                  </Button>
                 </div>
-                {s.rarity !== 0 && (
-                  <Slider
-                    className="w-full"
-                    defaultValue={[1]}
-                    step={1}
-                    min={1}
-                    max={6}
-                    onValueChange={(newValue: Array<number>) =>
-                      updateLevel(newValue[0], s.id)
-                    }
-                  ></Slider>
-                )}
-              </Item>
-            ))
-        )}
-      </ItemGroup>
-      <ItemGroup className="flex-row overflow-x-scroll gap-1 p-2 sm:max-w-5/6 w-full mx-auto">
-        {filteredPotentials.map((p) => (
-          <Item
-            onClick={() => {
-              addPotential(p)
-            }}
-            data-disabled={coreExceed}
-            className={`rarity-${p.rarity} p-0`}
-            key={p.id}
-          >
-            <ResponsivePotential
-              key={p.imgId + p.id}
-              bgSrc={`./ss-vestige/vestige_${p.rarity}.png`}
-              iconSrc={`https://res.cloudinary.com/dafqr01it/image/upload/v1763084273/ss/potential/${p.imgId}_A.png`}
-              name={p.name}
-            />
-          </Item>
-        ))}
-      </ItemGroup>
+              ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        </PopoverContent>
+      </Popover>
+
+      <ScrollArea className="w-full">
+        <div className="flex min-h-[170px] gap-1 bg-slate-300/90 p-2 rounded-sm">
+          {!selected || selected.length === 0 ? (
+            <div className="text-center self-center w-full">
+              Please choose potentials
+            </div>
+          ) : (
+            selected
+              .sort((a, b) => a.rarity - b.rarity)
+              .map((s) => (
+                <div
+                  key={'selected' + s.id}
+                  className="flex flex-wrap shrink-0 py-2"
+                >
+                  <div className="relative">
+                    <ResponsivePotential
+                      key={'selected' + s.imgId + s.id}
+                      bgSrc={`./ss-vestige/vestige_${s.rarity}.png`}
+                      iconSrc={`https://res.cloudinary.com/dafqr01it/image/upload/v1763084273/ss/potential/${s.imgId}_A.png`}
+                      name={s.name}
+                    />
+                    {s.rarity !== 0 && (
+                      <div className="absolute -top-0.5 left-3 text-sm font-semibold text-indigo-500">
+                        {s.level}
+                      </div>
+                    )}
+                    <Button
+                      variant="destructive"
+                      size="icon"
+                      className="absolute -top-1 -right-1 rounded-full size-5 border"
+                      onClick={() => removePotential(s.id)}
+                    >
+                      <X />
+                    </Button>
+                  </div>
+                  {s.rarity !== 0 && (
+                    <Slider
+                      className="w-full"
+                      defaultValue={[1]}
+                      step={1}
+                      min={1}
+                      max={6}
+                      onValueChange={(newValue: Array<number>) =>
+                        updateLevel(newValue[0], s.id)
+                      }
+                    ></Slider>
+                  )}
+                </div>
+              ))
+          )}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
   )
 }
