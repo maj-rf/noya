839c2e16cb85483673ac42a7a6cad60026e094fd
maj-rf <rmkfermin@gmail.com>
2025-12-01 21:57:59 +0800

    feat: convert preview to table

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -15,47 +15,83 @@ const PreviewRow = ({
   const selectedMap = useTrekkerStore((state) => state.potentials[slot])
   const potentials = useMemo(() => Object.values(selectedMap), [selectedMap])
 
-  const sortedPotentials = potentials.sort((a, b) => {
-    const order = ['Core', 'Medium', 'Optional'] as const
-    return order.indexOf(a.priority) - order.indexOf(b.priority)
-  })
+  const grouped = potentials.reduce(
+    (acc, item) => {
+      acc[item.priority].push(item)
+      return acc
+    },
+    {
+      Core: [] as typeof potentials,
+      Medium: [] as typeof potentials,
+      Optional: [] as typeof potentials,
+    },
+  )
 
   return (
-    <article>
-      <div className="bg-blue-900">
-        <h1 className="px-4 text-left text-white">
-          {slot === 'main' ? 'Main' : 'Support'}
-        </h1>
-      </div>
-      <div className="flex items-center gap-2 px-2 py-4">
+    <tr>
+      <td className="px-4 py-4 border-b">
         <div className="h-[125px] w-[100px]">
           {avatar && <SSAvatar char={avatar} />}
         </div>
-        <ul className="gap-2 flex-1 flex w-full flex-wrap">
-          {potentials.length === 0 ? (
-            <li className="text-center text-white">No Chosen potential</li>
-          ) : (
-            sortedPotentials.map((p) => {
-              return (
-                <li key={'preview' + p.id} className="relative inline-block">
-                  <ResponsivePotential
-                    rarity={p.rarity}
-                    imgId={p.imgId}
-                    name={p.name}
-                    subIcon={p.subIcon}
-                  />
-                  {p.rarity !== 0 && (
-                    <div className="absolute -top-0.5 left-3 text-sm text-center font-semibold text-indigo-500">
-                      {p.level}
-                    </div>
-                  )}
-                </li>
-              )
-            })
-          )}
+      </td>
+      <td className="px-1 py-4 border-b">
+        <ul className="flex flex-wrap gap-1 w-[200px] items-start justify-center">
+          {grouped.Core.map((p) => (
+            <li key={'preview' + p.id} className="relative">
+              <ResponsivePotential
+                rarity={p.rarity}
+                imgId={p.imgId}
+                name={p.name}
+                subIcon={p.subIcon}
+              />
+              {p.rarity !== 0 && (
+                <div className="absolute -top-0.5 left-3 text-sm text-center font-semibold text-indigo-500">
+                  {p.level}
+                </div>
+              )}
+            </li>
+          ))}
         </ul>
-      </div>
-    </article>
+      </td>
+      <td className="px-1 py-4 border-b">
+        <ul className="flex flex-wrap gap-1 w-[200px] items-start justify-center">
+          {grouped.Medium.map((p) => (
+            <li key={'preview' + p.id} className="relative">
+              <ResponsivePotential
+                rarity={p.rarity}
+                imgId={p.imgId}
+                name={p.name}
+                subIcon={p.subIcon}
+              />
+              {p.rarity !== 0 && (
+                <div className="absolute -top-0.5 left-3 text-sm text-center font-semibold text-indigo-500">
+                  {p.level}
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </td>
+      <td className="px-1 py-4 border-b">
+        <ul className="flex flex-wrap gap-1 w-[200px] justify-center items-center">
+          {grouped.Optional.map((p) => (
+            <li key={'preview' + p.id} className="relative">
+              <ResponsivePotential
+                rarity={p.rarity}
+                imgId={p.imgId}
+                name={p.name}
+                subIcon={p.subIcon}
+              />
+              {p.rarity !== 0 && (
+                <div className="absolute -top-0.5 left-3 text-sm text-center font-semibold text-indigo-500">
+                  {p.level}
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </td>
+    </tr>
   )
 }
 
@@ -68,10 +104,22 @@ export const Preview = ({
 }) => {
   return (
     <div className="h-0 overflow-hidden">
-      <section ref={ref} className="bg-slate-600 w-2xl" id="preview">
-        <PreviewRow avatar={avatar.main} slot="main" />
-        <PreviewRow avatar={avatar.sub1} slot="sub1" />
-        <PreviewRow avatar={avatar.sub2} slot="sub2" />
+      <section ref={ref} className="w-3xl" id="preview">
+        <table className="w-full bg-slate-800 shadow-lg rounded-lg overflow-hidden table-auto border-collapse">
+          <thead className="bg-blue-900 text-white">
+            <tr>
+              <th className="px-6 py-2 text-center font-semibold">Trekker</th>
+              <th className="px-6 py-2 text-center font-semibold">Core</th>
+              <th className="px-6 py-2 text-center font-semibold">Medium</th>
+              <th className="px-6 py-2 text-center font-semibold">Optional</th>
+            </tr>
+          </thead>
+          <tbody>
+            <PreviewRow avatar={avatar.main} slot="main" />
+            <PreviewRow avatar={avatar.sub1} slot="sub1" />
+            <PreviewRow avatar={avatar.sub2} slot="sub2" />
+          </tbody>
+        </table>
       </section>
     </div>
   )
