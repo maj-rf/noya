3c96f3d666deb9b930091a1343a4e9dcfec0deb5
maj-rf <rmkfermin@gmail.com>
2026-02-21 16:42:38 +0800

    feat: change preview style and change imageDownload default width

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -62,7 +62,7 @@ export function forceDisplayPlugin(): SnapdomPlugin {
       const target = clone.querySelector<HTMLElement>('#preview')
       if (target) {
         // target.style.setProperty('display', 'block', 'important')
-        target.style.width = 1440 + 'px'
+        target.style.width = 1600 + 'px'
       }
     },
   }
@@ -72,9 +72,8 @@ export async function downloadImage(element: HTMLElement | null) {
   if (!element) return
   const result = await snapdom(element, {
     plugins: [forceDisplayPlugin],
-    width: 1440,
+    width: 1600,
     embedFonts: true,
-    quality: 2,
     outerShadows: true,
   })
   await result.download({ filename: 'my-ss-build' })
