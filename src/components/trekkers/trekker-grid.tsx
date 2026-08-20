bfa880632735630b80294a2ee9fdc6ccbe539ac8
maj-rf <rmkfermin@gmail.com>
2026-08-20 11:02:33 +0800

    feat: implement working add and remove pots, extract SingleSelected component from the list

--- src/components/trekkers/trekker-grid.tsx
+++ src/components/trekkers/trekker-grid.tsx
@@ -1,6 +1,6 @@
 import { getRouteApi } from '@tanstack/react-router'
 import { useShallow } from 'zustand/shallow'
-import { MinusIcon, PlusIcon } from 'lucide-react'
+import { MinusIcon, ThumbsUpIcon } from 'lucide-react'
 import ResponsivePotential from '../potentials/responsive-potential'
 import {
   Select,
@@ -11,7 +11,7 @@ import {
 } from '../ui/select'
 import { Button } from '../ui/button'
 import { SSTrekker } from './ss-trekker'
-import type { Slot } from '@/types'
+import type { PotentialPriority, SSPotential, Slot } from '@/types'
 import { useLangStore, usePotentialStore, useTrekkerStore } from '@/lib/store'
 import { MAX_LEVEL } from '@/lib/utils'
 
@@ -20,21 +20,13 @@ type SSPotentialsProps = {
   type: 'main' | 'support'
 }
 
-export function TrekkerGrid({ slot, type }: SSPotentialsProps) {
-  const routeApi = getRouteApi('__root__')
-  const { potentials: fetchedPotentials, characters } = routeApi.useLoaderData()
+function SingleSelected({ slot, p }: { slot: Slot; p: SSPotential }) {
+  const sel = usePotentialStore((s) =>
+    s.potentials[slot].find((sp) => sp.id === p.id),
+  )
   const lang = useLangStore((s) => s.lang)
-  const trekkerId = useTrekkerStore((s) => s.trekkers[slot])
   const selected = usePotentialStore(
-    useShallow((s) =>
-      s.potentials[slot]
-        .sort((a, b) => {
-          const aRank = a.rarity === 0 ? 0 : 1
-          const bRank = b.rarity === 0 ? 0 : 1
-          return aRank - bRank // rarity 0 first, 1 & 2 after
-        })
-        .map((p) => p.id),
-    ),
+    useShallow((s) => s.potentials[slot].map((sp) => sp.id)),
   )
   const coreExceed = usePotentialStore((s) => {
     const entries = Object.values(s.potentials[slot])
@@ -48,8 +40,116 @@ export function TrekkerGrid({ slot, type }: SSPotentialsProps) {
 
   const add = usePotentialStore((s) => s.addPotential)
   const remove = usePotentialStore((s) => s.removePotential)
-  const reorder = usePotentialStore((s) => s.reorder)
+  const updateLevel = usePotentialStore((s) => s.updateLevel)
+  const updatePriority = usePotentialStore((s) => s.updatePriority)
+  // const reorder = usePotentialStore((s) => s.reorder)
 
+  return (
+    <div key={p.id} className="relative w-full">
+      {selected.includes(p.id) ? (
+        <Button
+          className="absolute -top-1 -right-0.5 m-auto z-10 rounded-full"
+          size="icon-xs"
+          onClick={() => remove(slot, p.id)}
+        >
+          <MinusIcon />
+        </Button>
+      ) : (
+        <Button
+          className="absolute -top-1 -right-0.5 m-auto z-10 rounded-full"
+          size="icon-xs"
+          onClick={() => add(slot, p)}
+          disabled={p.rarity === 0 && coreExceed}
+        >
+          <ThumbsUpIcon />
+        </Button>
+      )}
+      <div
+        data-selected={selected.includes(p.id)}
+        className="opacity-60 data-[selected=true]:opacity-100"
+      >
+        <ResponsivePotential
+          size="w-20 sm:w-full"
+          rarity={p.rarity}
+          imgId={p.imgId}
+          name={p.name[lang]}
+        />
+      </div>
+      {p.rarity !== 0 && (
+        <div className="absolute top-0 bg-white flex w-full rounded-sm">
+          {sel && (
+            <Select
+              value={sel.level ? String(sel.level) : undefined}
+              onValueChange={(value) => updateLevel(slot, p.id, value)}
+              defaultValue={'6'}
+            >
+              <SelectTrigger
+                className="text-[10px] justify-start gap-0.5 pl-1 pr-0 border-none shadow-none ring-0 focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 text-mauve-800 bg-transparent data-[size=sm]:h-3"
+                size="sm"
+              >
+                <SelectValue placeholder="Level" />
+              </SelectTrigger>
+              <SelectContent align="start">
+                <SelectItem className="text-[10px]" value="1+">
+                  1+
+                </SelectItem>
+                {[...Array(MAX_LEVEL)].map((_, index) => (
+                  <SelectItem
+                    className="text-[10px]"
+                    key={'level ' + String(index + 1)}
+                    value={String(index + 1)}
+                  >
+                    {index + 1}
+                  </SelectItem>
+                ))}
+                <SelectItem className="text-[10px]" value={`${MAX_LEVEL}+`}>
+                  {`${MAX_LEVEL}+`}
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          )}
+          <div className="bg-white w-fit px-1">
+            {sel && (
+              <Select
+                value={sel.priority}
+                onValueChange={(value) => {
+                  updatePriority(slot, sel.id, value as PotentialPriority)
+                }}
+                defaultValue="Medium"
+              >
+                <SelectTrigger
+                  className="text-[10px] justify-start gap-0.5 border-none shadow-none ring-0 focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 bg-transparent p-0 text-mauve-800 data-[size=sm]:h-3.25"
+                  size="sm"
+                >
+                  <SelectValue placeholder="Priority" />
+                </SelectTrigger>
+                <SelectContent align="start">
+                  <SelectItem className="text-[10px]" value="Core">
+                    Core
+                  </SelectItem>
+                  <SelectItem className="text-[10px]" value="Medium">
+                    Med
+                  </SelectItem>
+                  <SelectItem className="text-[10px]" value="Low">
+                    Low
+                  </SelectItem>
+                  <SelectItem className="text-[10px]" value="Optional">
+                    Opt
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export function TrekkerGrid({ slot, type }: SSPotentialsProps) {
+  const routeApi = getRouteApi('__root__')
+  const { potentials: fetchedPotentials } = routeApi.useLoaderData()
+  const trekkerId = useTrekkerStore((s) => s.trekkers[slot])
   if (!trekkerId) return
   const potentialList = fetchedPotentials[trekkerId]
   const potentials = Object.values(potentialList)
@@ -64,144 +164,15 @@ export function TrekkerGrid({ slot, type }: SSPotentialsProps) {
           <div className="col-span-2 row-span-2">
             <SSTrekker id={trekkerId} />
           </div>
-
           {filteredPotentials.slice(0, 4).map((s) => {
             const p = potentialList[s.id]
-            return (
-              <div key={p.id} className="relative w-full">
-                {selected.includes(p.id) ? (
-                  <Button
-                    className="absolute top-0 right-0 m-auto z-10 rounded-full"
-                    size="icon-xs"
-                    onClick={() => remove(slot, p.id)}
-                  >
-                    <MinusIcon />
-                  </Button>
-                ) : (
-                  <Button
-                    className="absolute top-0 right-0 m-auto z-10 rounded-full"
-                    size="icon-xs"
-                    onClick={() => add(slot, p)}
-                    disabled={coreExceed}
-                  >
-                    <PlusIcon />
-                  </Button>
-                )}
-
-                <div
-                  data-selected={selected.includes(p.id)}
-                  className="opacity-60 data-[selected=true]:opacity-100"
-                >
-                  <ResponsivePotential
-                    size="w-20 sm:w-full"
-                    rarity={p.rarity}
-                    imgId={p.imgId}
-                    name={p.name[lang]}
-                  />
-                </div>
-              </div>
-            )
+            return <SingleSelected key={p.id} slot={slot} p={p} />
           })}
         </div>
-
         <div className="grid grid-cols-[80px_80px_80px] auto-rows-[102px] gap-1">
           {filteredPotentials.slice(4).map((s) => {
             const p = potentialList[s.id]
-            return (
-              <div key={p.id} className="relative w-full">
-                {selected.includes(p.id) ? (
-                  <Button
-                    className="absolute top-0 right-0 m-auto z-10 rounded-full"
-                    size="icon-xs"
-                    onClick={() => remove(slot, p.id)}
-                  >
-                    <MinusIcon />
-                  </Button>
-                ) : (
-                  <Button
-                    className="absolute top-0 right-0 m-auto z-10 rounded-full"
-                    size="icon-xs"
-                    onClick={() => add(slot, p)}
-                    disabled={coreExceed}
-                  >
-                    <PlusIcon />
-                  </Button>
-                )}
-                <div
-                  data-selected={selected.includes(p.id)}
-                  className="opacity-60 data-[selected=true]:opacity-100"
-                >
-                  <ResponsivePotential
-                    size="w-20 sm:w-full"
-                    rarity={p.rarity}
-                    imgId={p.imgId}
-                    name={p.name[lang]}
-                  />
-                </div>
-                <div className="absolute top-0 bg-transparent flex rounded-sm">
-                  <Select
-                    value={s.level ? String(s.level) : undefined}
-                    onValueChange={(value) => updateLevel(slot, s.id, value)}
-                    defaultValue={'6'}
-                  >
-                    <SelectTrigger
-                      className="text-[10px] justify-start gap-0.5 pl-2 pr-0 border-none shadow-none ring-0 focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 text-mauve-800 bg-transparent data-[size=sm]:h-3"
-                      size="sm"
-                    >
-                      <SelectValue placeholder="Level" />
-                    </SelectTrigger>
-                    <SelectContent align="start">
-                      <SelectItem className="text-[10px]" value="1+">
-                        1+
-                      </SelectItem>
-                      {[...Array(MAX_LEVEL)].map((_, index) => (
-                        <SelectItem
-                          className="text-[10px]"
-                          key={'level ' + String(index + 1)}
-                          value={String(index + 1)}
-                        >
-                          {index + 1}
-                        </SelectItem>
-                      ))}
-                      <SelectItem
-                        className="text-[10px]"
-                        value={`${MAX_LEVEL}+`}
-                      >
-                        {`${MAX_LEVEL}+`}
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <div className="bg-white w-fit px-1">
-                    <Select
-                      // value={s.priority}
-                      onValueChange={() => {}}
-                      defaultValue="Medium"
-                    >
-                      <SelectTrigger
-                        className="text-[10px] justify-start gap-0.5 border-none shadow-none ring-0 focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 bg-transparent p-0 text-mauve-800 data-[size=sm]:h-3.25"
-                        size="sm"
-                      >
-                        <SelectValue placeholder="Priority" />
-                      </SelectTrigger>
-                      <SelectContent align="start">
-                        <SelectItem className="text-[10px]" value="Core">
-                          Core
-                        </SelectItem>
-                        <SelectItem className="text-[10px]" value="Medium">
-                          Med
-                        </SelectItem>
-                        <SelectItem className="text-[10px]" value="Low">
-                          Low
-                        </SelectItem>
-                        <SelectItem className="text-[10px]" value="Optional">
-                          Opt
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                </div>
-              </div>
-            )
+            return <SingleSelected key={p.id} slot={slot} p={p} />
           })}
         </div>
       </div>
